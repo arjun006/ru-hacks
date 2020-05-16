84f791d6ee2104eaefb68b8ce191396b44a427b2
Shira <wisdomkangngo@gmail>
2020-05-16 04:59:00 -0400

    updated

--- server/server.js
+++ server/server.js
@@ -10,11 +10,38 @@ const app = express();
   });
 
   // Performs label detection on the image file
+  async function image(req,res){
   const [result] = await client.labelDetection('./meme.png');
   const labels = result.labelAnnotations;
   console.log('Labels:');
   labels.forEach(label => console.log(label.description));
+  }
+  //Call function
+image();
 
+
+//nutritionix (install unirest)
+var unirest = require('unirest');
+
+//searches the food
+var req = unirest("GET", "https://nutritionix-api.p.rapidapi.com/v1_1/search/cheddar%2520cheese");
+
+req.query({
+	"fields": "item_name%2Cnf_calories%2Cnf_total_fat"
+});
+
+req.headers({
+	"x-rapidapi-host": "nutritionix-api.p.rapidapi.com",
+	"x-rapidapi-key": "2ed1a5be6emshc43763ec17ac9e3p1b4e24jsn9d02e0625f12",
+	"useQueryString": true
+});
+
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+	console.log(res.body);
+});
 
 app.get("/", (req, res) => {
     res.send('server started');
