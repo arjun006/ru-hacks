ee34795741347dac141be5d8f43eddf4ae23c02e
Shira <wisdomkangngo@gmail>
2020-05-16 19:32:57 -0400

    updated

--- server/server.js
+++ server/server.js
@@ -46,7 +46,7 @@ var options = {
 
 function callback(error, response, body) {
     if (!error && response.statusCode == 200) {
-        console.log(body);
+        console.log(JSON.parse(body));
     }
 }
 
