08adc1419dbb47edbf41ce27370616e4b3bb1c55
Shira <wisdomkangngo@gmail>
2020-05-16 22:52:39 -0400

    updated

--- server/vision.js
+++ server/vision.js
@@ -1,22 +1,45 @@
 const express = require('express');
 
 //Cloud Vision
-descriptions = [];
+
+
 // Performs label detection on the image file
 async function image(req,res){
+    descriptions = [];
     const vision = require('@google-cloud/vision');
   // Creates a client
     const client = new vision.ImageAnnotatorClient({
       keyFilename: 'API-vision.json'
     });
     
-    const [result] = await client.labelDetection('./meme.png');
+    const [result] = await client.labelDetection('Fried Rice.jpg');
     const labels = result.labelAnnotations;
   //Stores result into array
     labels.forEach(label => descriptions.push(label.description));
-    console.log(descriptions)
+    //console.log(descriptions)
+    return descriptions
     }
-  //Call function
-  image();
+  //Filters Label for client's to choose from
+  function filter(descriptions)
+  {    
+      var fdescription = [];
+  for (var i = 0; i < descriptions.length; i++)
+  {
+
+      if (descriptions[i] != "Food" && descriptions[i] != "Dish" && descriptions[i] != "Cuisine" && descriptions[i] != "Ingredient")
+      {
+          fdescription.push(descriptions[i]);
+      }
+      
+  }
+  console.log(fdescription)
+  return fdescription
+}
+
+//Function call
+  image()
+  filter(descriptions)
+
+
 
 module.export = 'vision.js'
