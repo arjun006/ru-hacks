228d4e26f836acf324fafe6f45d3fb4326f35a66
Shira <wisdomkangngo@gmail>
2020-05-17 02:40:14 -0400

    updated

--- server/routes/api/exampleApi.js
+++ server/routes/api/exampleApi.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const multer = require("multer");
 const router = express.Router();
 
 router.get('/',(req,res)=>{
@@ -6,7 +7,40 @@ router.get('/',(req,res)=>{
 })
 
 router.post('/', (req, res)=>{
+const handleError = (err, res) => {
+  res
+    .status(500)
+    .contentType("text/plain")
+    .end("Oops! Something went wrong!");
+};
+app.get(
+    //getting word from server
+)
+app.post(
+  "/upload",
+  upload.single("file" /* name attribute of <file> element in your form */),
+  (req, res) => {
+    if (path.extname(req.file.originalname).toLowerCase() === ".png" || path.extname(req.file.originalname).toLowerCase() === ".jpg") {
+      fs.rename(tempPath, targetPath, err => {
+        if (err) return handleError(err, res);
 
+        res
+          .status(200)
+          .contentType("text/plain")
+          .end("File uploaded!");
+      });
+    } else {
+      fs.unlink(tempPath, err => {
+        if (err) return handleError(err, res);
+
+        res
+          .status(403)
+          .contentType("text/plain")
+          .end("Only .png/.jpg files are allowed!");
+      });
+    }
+  }
+);
 })
 
 module.exports = router;
